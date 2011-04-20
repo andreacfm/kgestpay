1f688912639055de6ea3c0f4febbdf7b3772a9e2
Andrea Campolonghi <acampolonghi@gmail.com>
2011-04-20 10:50:11 +0200

    check required args in callPagamS2S

--- lib/kgestpay.rb
+++ lib/kgestpay.rb
@@ -1,9 +1,22 @@
+#encoding : utf-8
 module Kemen
 
   class KGestPay
 
-    def initialize
-      # passa e salva i dati tipo login password etc
+    def initialize(args)
+      @shopLogin = args[:shopLogin]
+      super
+    end
+
+    def callPagamS2S(args)
+      #Required instance variable
+      raise(RuntimeError.new("Variable @shopLogin has not been initialized")) if instance_variable_get(:@shopLogin).nil?
+
+      #Required arguments
+      [:uicCode,:amount,:shopTransactionId,:cardNumber,:expiryMonth,:expiryYear].each do |arg|
+        raise(ArgumentError.new("Parameter [:#{arg}] is required but has not been passed")) if args[arg.to_sym].nil?
+      end
+
     end
 
   end
--- test/kgestpay_test.rb
+++ test/kgestpay_test.rb
@@ -1,3 +1,5 @@
+#encoding : utf-8
+
 require 'rubygems'
 require 'test/unit'
 require 'rails/all'
@@ -7,11 +9,24 @@ require "kgestpay"
 class KGestPayTest < Test::Unit::TestCase
 
   def setup
-    @k = Kemen::KGestPay.new
+    @shopLogin = 'GESPAY51954'
   end
 
-  def test_craete_instance
-    assert_not_nil @k
+  def test_shoplogin_saved_as_instance_variable
+    k = Kemen::KGestPay.new(
+      :shopLogin => @shopLogin
+    )
+    assert_equal(k.instance_variable_get(:@shopLogin),@shopLogin)
+  end
+
+  def test_callPagamS2S_args
+    k = Kemen::KGestPay.new(:shop => 'shop')
+    assert_raise(RuntimeError){k.callPagamS2S(:amount => 1)}
+
+    k = Kemen::KGestPay.new(:shopLogin => @shopLogin)
+    assert_raise(ArgumentError){k.callPagamS2S(:amount => 1)}
+    assert_raise(ArgumentError){k.callPagamS2S(:uicCode => 1)}
+
   end
 
 end
