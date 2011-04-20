3402b493f2a4ac81b0b159edc25c498fb8fac047
Andrea Campolonghi <acampolonghi@gmail.com>
2011-04-20 12:22:05 +0200

    add endpoint instance variable add ws creation on initiliaze

--- lib/kgestpay.rb
+++ lib/kgestpay.rb
@@ -2,20 +2,27 @@
 module Kemen
 
   class KGestPay
+    require 'soap/wsdlDriver'
 
     def initialize(args)
+      raise(ArgumentError.new(" Missing argument [:shopLogin] Variable @shopLogin cannot be initiliazed.")) if args[:shopLogin].nil?
+      raise(ArgumentError.new(" Missing argument [:endpoint] Variable @endpoint cannot be initiliazed.")) if args[:endpoint].nil?
       @shopLogin = args[:shopLogin]
+      @endpoint = args[:endpoint]
+      #create the ws instance
+      @ws = SOAP::WSDLDriverFactory.new("#{@endpoint}?WSDL").create_rpc_driver
       super
     end
 
     def callPagamS2S(args)
-      #Required instance variable
-      raise(RuntimeError.new("Variable @shopLogin has not been initialized")) if instance_variable_get(:@shopLogin).nil?
 
       #Required arguments
       [:uicCode,:amount,:shopTransactionId,:cardNumber,:expiryMonth,:expiryYear].each do |arg|
         raise(ArgumentError.new("Parameter [:#{arg}] is required but has not been passed")) if args[arg.to_sym].nil?
       end
+
+      #invoke the gestpay webservice method and return the result
+      @ws.callPagamS2S(args).callPagamS2SResult.gestPayS2S
 
     end
 
--- test/kgestpay_test.rb
+++ test/kgestpay_test.rb
@@ -10,23 +10,30 @@ class KGestPayTest < Test::Unit::TestCase
 
   def setup
     @shopLogin = 'GESPAY51954'
+    @endpoint = 'https://testecomm.sella.it/gestpay/gestpayws/WSs2s.asmx'
+    @k = Kemen::KGestPay.new(:shopLogin => @shopLogin, :endpoint => @endpoint)
+
   end
 
   def test_shoplogin_saved_as_instance_variable
-    k = Kemen::KGestPay.new(
-      :shopLogin => @shopLogin
-    )
-    assert_equal(k.instance_variable_get(:@shopLogin),@shopLogin)
+    assert_equal(@k.instance_variable_get(:@shopLogin),@shopLogin)
+    assert_equal(@k.instance_variable_get(:@endpoint),@endpoint)
+    assert_equal(@k.instance_variable_get(:@ws).proxy.endpoint_url,@endpoint)
   end
 
   def test_callPagamS2S_args
-    k = Kemen::KGestPay.new(:shop => 'shop')
-    assert_raise(RuntimeError){k.callPagamS2S(:amount => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:amount => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryMonth => 1,:expiryYear => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:uicCode => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryMonth => 1,:expiryYear => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:uicCode => 1,:amount => 1,:cardNumber => 1,:expiryMonth => 1,:expiryYear => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:uicCode => 1,:amount => 1,:shopTransactionId => 1,:expiryMonth => 1,:expiryYear => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:uicCode => 1,:amount => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryYear => 1)}
+    assert_raise(ArgumentError){@k.callPagamS2S(:uicCode => 1,:amount => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryMonth => 1)}
+    assert_nothing_raised(ArgumentError){@k.callPagamS2S(:uicCode => 1,:amount => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryMonth => 1,:expiryYear => 1)}
+  end
 
-    k = Kemen::KGestPay.new(:shopLogin => @shopLogin)
-    assert_raise(ArgumentError){k.callPagamS2S(:amount => 1)}
-    assert_raise(ArgumentError){k.callPagamS2S(:uicCode => 1)}
-
+  def test_callPagamS2S
+    resp = @k.callPagamS2S(:uicCode => 1,:amount => 1,:shopTransactionId => 1,:cardNumber => 1,:expiryMonth => 1,:expiryYear => 1)
+    assert_equal('KO',resp.transactionResult)
   end
 
 end
